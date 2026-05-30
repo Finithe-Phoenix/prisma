915da5dd4b1a7bc57e6ee4d83d35d2ff1f2632c7
Finithe-Phoenix <37785263+Finithe-Phoenix@users.noreply.github.com>
2026-05-30 10:22:53 -0600

    fix: resolve duplicate IR integration definitions

--- core/src/ir/operation.cpp
+++ core/src/ir/operation.cpp
@@ -12,9 +12,6 @@ bool operator==(const Constant& a, const Constant& b) noexcept {
 bool operator==(const LoadReg& a, const LoadReg& b) noexcept {
     return a.reg == b.reg && a.size == b.size;
 }
-bool operator==(const LoadSegBase& a, const LoadSegBase& b) noexcept {
-    return a.segment == b.segment;
-}
 bool operator==(const StoreReg& a, const StoreReg& b) noexcept {
     return a.reg == b.reg && a.value == b.value && a.size == b.size;
 }
@@ -23,15 +20,6 @@ bool operator==(const LoadSegBase& a, const LoadSegBase& b) noexcept {
 }
 bool operator==(const BinOp& a, const BinOp& b) noexcept {
     return a.op == b.op && a.lhs == b.lhs && a.rhs == b.rhs && a.size == b.size;
-}
-bool operator==(const Extend& a, const Extend& b) noexcept {
-    return a.value == b.value
-        && a.from_size == b.from_size
-        && a.to_size == b.to_size
-        && a.is_signed == b.is_signed;
-}
-bool operator==(const Truncate& a, const Truncate& b) noexcept {
-    return a.value == b.value && a.to_size == b.to_size;
 }
 bool operator==(const Compare& a, const Compare& b) noexcept {
     return a.cc == b.cc && a.lhs == b.lhs && a.rhs == b.rhs && a.size == b.size;
@@ -52,9 +40,6 @@ bool operator==(const LoadMemTSO& a, const LoadMemTSO& b) noexcept {
 bool operator==(const StoreMemTSO& a, const StoreMemTSO& b) noexcept {
     return a.addr == b.addr && a.value == b.value && a.size == b.size;
 }
-bool operator==(const GuestPc& a, const GuestPc& b) noexcept {
-    return a.pc == b.pc;
-}
 bool operator==(const Jump& a, const Jump& b) noexcept {
     return a.target_block == b.target_block;
 }
@@ -63,9 +48,6 @@ bool operator==(const JumpReg& a, const JumpReg& b) noexcept {
 }
 bool operator==(const CondJump& a, const CondJump& b) noexcept {
     return a.cond == b.cond && a.if_true == b.if_true && a.if_false == b.if_false;
-}
-bool operator==(const CondJumpFlags& a, const CondJumpFlags& b) noexcept {
-    return a.cc == b.cc && a.if_true == b.if_true && a.if_false == b.if_false;
 }
 bool operator==(const Return&, const Return&) noexcept {
     return true;
@@ -104,20 +86,6 @@ bool operator==(const CondJumpRel& a, const CondJumpRel& b) noexcept {
         && a.target_guest_pc == b.target_guest_pc
         && a.fallthrough_guest_pc == b.fallthrough_guest_pc;
 }
-
-bool operator==(const CallRel& a, const CallRel& b) noexcept {
-    return a.target_guest_pc == b.target_guest_pc
-        && a.return_guest_pc == b.return_guest_pc;
-}
-bool operator==(const CallReg& a, const CallReg& b) noexcept {
-    return a.target == b.target && a.return_guest_pc == b.return_guest_pc;
-}
-bool operator==(const RetAdjusted& a, const RetAdjusted& b) noexcept {
-    return a.pop_bytes == b.pop_bytes;
-}
-bool operator==(const Cpuid&, const Cpuid&) noexcept   { return true; }
-bool operator==(const Syscall&, const Syscall&) noexcept { return true; }
-bool operator==(const Trap& a, const Trap& b) noexcept { return a.kind == b.kind; }
 bool operator==(const Extend& a, const Extend& b) noexcept {
     return a.value == b.value
         && a.from_size == b.from_size
@@ -126,9 +94,6 @@ bool operator==(const Extend& a, const Extend& b) noexcept {
 }
 bool operator==(const Truncate& a, const Truncate& b) noexcept {
     return a.value == b.value && a.to_size == b.to_size;
-}
-bool operator==(const Fence& a, const Fence& b) noexcept {
-    return a.kind == b.kind;
 }
 bool operator==(const GuestPc& a, const GuestPc& b) noexcept {
     return a.pc == b.pc;
